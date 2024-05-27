094f360b7cb94efa87f2fa5422fed49969a6a74b
Anderson Oliveira <andersonreinilson@hotmail.com>
2024-05-27 05:15:06 -0300

    feat: alteração na rota principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,6 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
     path: 'list-pokemons',
     loadChildren: () => import('./pages/list-pokemons/list-pokemons.module').then( m => m.ListPokemonsPageModule)
   },
@@ -22,7 +13,12 @@ const routes: Routes = [
   {
     path: 'favorites-pokemons',
     loadChildren: () => import('./pages/favorites-pokemons/favorites-pokemons.module').then( m => m.FavoritesPokemonsPageModule)
-  }
+  },
+  {
+    path: '',
+    redirectTo: 'list-pokemons',
+    pathMatch: 'full'
+  },
 ];
 
 @NgModule({
