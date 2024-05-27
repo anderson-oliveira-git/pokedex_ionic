beeefb1b3394de0a3c8a710167065c0ea6c33ab2
Anderson Oliveira <andersonreinilson@hotmail.com>
2024-05-27 05:13:56 -0300

    feat: tela de listagem de pokemons implementada

--- src/app/pages/list-pokemons/list-pokemons.module.ts
+++ src/app/pages/list-pokemons/list-pokemons.module.ts
@@ -7,14 +7,17 @@ import { IonicModule } from '@ionic/angular';
 import { ListPokemonsPageRoutingModule } from './list-pokemons-routing.module';
 
 import { ListPokemonsPage } from './list-pokemons.page';
+import { PokemonCardModule } from 'src/app/components/pokemon-card/pokemon-card.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ListPokemonsPageRoutingModule
+    ListPokemonsPageRoutingModule,
+    PokemonCardModule
   ],
   declarations: [ListPokemonsPage]
 })
+
 export class ListPokemonsPageModule {}
--- src/app/pages/list-pokemons/list-pokemons.page.ts
+++ src/app/pages/list-pokemons/list-pokemons.page.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Pokemon } from 'src/app/models/pokemon';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-list-pokemons',
   templateUrl: './list-pokemons.page.html',
   styleUrls: ['./list-pokemons.page.scss'],
 })
+
 export class ListPokemonsPage implements OnInit {
 
-  constructor() { }
+  public pokemons: Pokemon[];
+
+  constructor(
+    private pokemonService: PokemonService
+  ) {
+    this.pokemons = [];
+  }
 
   ngOnInit() {
+    this.morePokemon();
+  }
+
+  morePokemon () {
+    const promise = this.pokemonService.getAllPokemons();
+
+    if (promise) {
+      promise.then((result: Pokemon[]) => {
+        this.pokemons = this.pokemons.concat(result);
+
+        console.log(this.pokemons);
+
+      });
+    }
   }
 
 }
