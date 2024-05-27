21e078478cec1cff67178ec62cf0b11f1b53fe52
Anderson Oliveira <andersonreinilson@hotmail.com>
2024-05-27 01:20:56 -0300

    build: habilita o plugin CapacitorHttp

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'pokedex_app',
-  webDir: 'www'
+  appName: 'pokedex_app_bsn',
+  webDir: 'www',
+  plugins: {
+    CapacitorHttp: {
+      enabled: true
+    }
+  }
 };
 
 export default config;
