00f87a743c2b0e78327e6880dac3e816fbeb584c
Anderson Oliveira <andersonreinilson@hotmail.com>
2024-05-28 06:07:37 -0300

    feat: adiciona float buttom na tela de favorites

--- src/app/pages/favorites-pokemons/favorites-pokemons.page.ts
+++ src/app/pages/favorites-pokemons/favorites-pokemons.page.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-favorites-pokemons',
   templateUrl: './favorites-pokemons.page.html',
   styleUrls: ['./favorites-pokemons.page.scss'],
 })
-export class FavoritesPokemonsPage implements OnInit {
+export class FavoritesPokemonsPage {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ){}
 
-  ngOnInit() {
+  navigateToListPokemons() {
+    this.router.navigate(['/list-pokemons']);
   }
-
 }
